2000f75c77a3f6522c7eefe2c454ed1e40093c5a
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <ronnie271713@gmail.com>
2024-10-12 21:30:40 +0530

    𝙷𝙰𝚁𝚁𝚈

--- VIPMUSIC/plugins/sudo/dev.py
+++ VIPMUSIC/plugins/sudo/dev.py
@@ -214,7 +214,7 @@ async def shellrunner(_, message: Message):
     await message.stop_propagation()
 
 
-__MODULE__ = "Deᴠ"
+__MODULE__ = "🧑🏻‍💻 Deᴠ 🧑🏻‍💻"
 __HELP__ = """
 🔰<b><u>Aᴅᴅ Aɴᴅ Rᴇᴍᴏᴠᴇ Sᴜᴅᴏ Usᴇʀ's:</u></b>
 
