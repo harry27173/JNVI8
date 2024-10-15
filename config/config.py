6929919c0ad5ed2a2773e2df7d80d75bc50e6ee6
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <ronnie271713@gmail.com>
2024-10-15 09:38:32 +0530

    𝙷𝙰𝚁𝚁𝚈

--- config/config.py
+++ config/config.py
@@ -211,7 +211,7 @@ autoclean = []
 
 START_IMG_URL = getenv(
     "START_IMG_URL",
-    "https://graph.org/file/0c2c5610c63e4bc13544c.jpg",
+    "https://envs.sh/Teo.jpg",
 )
 
 PING_IMG_URL = getenv(
