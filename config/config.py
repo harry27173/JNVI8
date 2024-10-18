495c19dd156fea1e850b247406c9e7da53c4ee86
𓆩◕🇭𝐀𝐑𝐑𝐘◕𓆪 =‌𐏓 𝄗⃝🇮🇳™ <ronnie271713@gmail.com>
2024-10-18 15:21:57 +0530

    𝙷𝙰𝚁𝚁𝚈

--- config/config.py
+++ config/config.py
@@ -74,7 +74,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6754480012").split())
+    map(int, getenv("OWNER_ID", "7129262488").split())
 )  # Input type must be interger
 
 
